262b4dea0dc163a0523679bd2b0b14c08ac04aef
Sulianto1992 <Sulianto1992@gmail.com>
2019-09-15 16:17:34 +0800

    Third Version

--- employee-dashboard-backend-spring/src/main/java/com/optimum/employeedashboard/EmployeedashboardApplication.java
+++ employee-dashboard-backend-spring/src/main/java/com/optimum/employeedashboard/EmployeedashboardApplication.java
@@ -2,8 +2,8 @@ package com.optimum.employeedashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
